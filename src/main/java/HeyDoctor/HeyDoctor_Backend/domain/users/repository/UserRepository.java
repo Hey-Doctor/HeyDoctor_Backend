be4473c182fb6bd1b82c8fa4bc04c6a288a1eb82
junjinyun <kjkj4142@naver.com>
2025-07-11 21:45:41 +0900

    remove : 기초 자료 삭제

--- src/main/java/HeyDoctor/HeyDoctor_Backend/domain/users/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package HeyDoctor.HeyDoctor_Backend.domain.users.repository;
-
-import HeyDoctor.HeyDoctor_Backend.domain.users.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/HeyDoctor/HeyDoctor_Backend/domain/users/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package HeyDoctor.HeyDoctor_Backend.domain.users.service;
-
-import HeyDoctor.HeyDoctor_Backend.domain.users.dto.UserResponse;
-
-public interface UserService {
-    UserResponse getMemberById(Long id);
-}
